4792e9b1ff0368bccc47e45f93521dca2d00c29a
Lauren Przywara <106774805+L-Prz@users.noreply.github.com>
2023-01-22 18:13:40 -0500

    Create recipes.py

---
+++ recipes.py
@@ -0,0 +1,233 @@
+my_recipes={
+        0:{
+            "name":"Buffalo Chicken Mac and Cheese",
+            "ingredients":["cheddar cheese", "milk", "elbow noodles", "chicken", "hot sauce", "butter", "scallions" ],
+            "cuisine":"American",
+            "protein":"chicken",
+            "diet":["none", "gluten_free"]
+            },
+        1:{
+            "name":"Keto White Pizza with Arugula and Balsamic Glaze",
+            "ingredients":["ricotta cheese", "parmigiano reggiano", "asiago cheese", "romano cheese", "dried basil", "dried oregano", "dried parsley", "garlic powder", "crushed red pepper", "bacon", "shallot", "arugula", "olive oil", "balsamic glaze"],
+            "cuisine":"Italian",
+            "protein":"bacon, cheese",
+            "diet":["keto"]
+        },
+        2:{
+            "name":"Chicken Caprese",
+            "ingredients":["olive oil", "white wine vinegar", "garlic powder", "dried oregano", "dried basil", "dried parsley", "onion powder", "crushed red pepper", "garlic", "lemon juice", "chicken", "fresh mozzarella", "tomatoes", "red onion", "balsamic vinegar", "basil", "balsamic glaze"],
+            "cuisine":"Italian",
+            "protein":"chicken",
+            "diet":["keto", "gluten_free", "none"]
+        },
+        3:{
+            "name":"Swedish Meatballs",
+            "ingredients":["ground pork", "mozzarella cheese", "eggs", "yellow onion", "dried parsley", "dried fennel", "paprika", "garlic powder", "celery powder", "butter", "cornstarch", "beef stock", "heavy cream", "soy sauce"],
+            "cuisine":"Swedish",
+            "protein":"pork",
+            "diet":["keto", "gluten_free", "none"]
+        },
+        4:{
+            "name":"Zuppa Toscana",
+            "ingredients":["bacon", "spicy italian chicken sausage", "yellow onion", "garlic", "chicken broth", "dried basil", "dried oregano", "crushed red pepper", "kale", "coconut milk", "parmigiano reggiano", "cauliflower gnocchi"],
+            "cuisine":"Italian",
+            "protein":"sausage",
+            "diet":["gluten_free", "none"]
+        },
+        5:{
+            "name":"Cabbage Roll Soup",
+            "ingredients":["butter", "olive oil", "yellow onion", "garlic", "ground beef", "sausage", "beef stock", "oregano", "smoked paprika", "garlic powder", "onion powder", "dried thyme", "can diced tomatoes", "can tomato paste", "parsley", "cabbage"],
+            "cuisine":"American",
+            "protein":"sausage",
+            "diet":["keto", "gluten_free", "none"]
+        },
+        6:{
+            "name":"Kale Salad with Crispy Tofu and Miso-Ginger Dressing",
+            "ingredients":["rice vinegar", "sesame oil", "miso paste", "ginger", "kale", "carrot", "firm tofu", "cornstarch", "avocado", "roasted cashews", "mandarin orange"],
+            "cuisine":"Asian",
+            "protein":"tofu",
+            "diet":["vegetarian", "gluten_free", "none"]
+        },
+        7:{
+            "name":"Buffalo Chicken Wings with Blue Cheese Dip",
+            "ingredients":["chicken wings", "flour", "cayenne", "sriracha", "maple syrup", "butter", "blue cheese crumbles", "mayo", "sour cream", "buttermilk"],
+            "cuisine":"American",
+            "protein":"chicken",
+            "diet":["none","gluten_free"]
+        },
+        8:{
+            "name":"Mujadara with Zhoug",
+            "ingredients":["garlic", "cilantro", "jalapeno", "ground cardamom", "cumin", "crushed red pepper", "lemon", "olive oil", "vidalia onion", "butter", "smoked paprika", "onion powder", "cayenne", "bay leaves", "veggie stock", "brown rice", "lentils", "sweet potato", "greek yogurt"],
+            "cuisine":"African",
+            "protein":"lentils",
+            "diet":["vegetarian", "gluten_free"]
+        },
+        9:{
+            "name":"Firey Vegan Taco Stew",
+            "ingredients":["yellow onion", "red bell pepper", "jalapeno", "veggie stock", "garlic", "cumin", "cayenne", "lentils", "can diced tomatoes", "cilantro", "avocado"],
+            "cuisine":"Mexican",
+            "protein":"lentils",
+            "diet":["vegetarian"]
+        },
+        10:{
+            "name":"Buffalo Chicken Salad",
+            "ingredients":["lettuce", "celery", "sour cream", "heavy whipping cream", "scallions", "garlic", "parsley", "dill", "lemon", "paprika", "chicken", "hot sauce", "butter"],
+            "cuisine":"American",
+            "protein":"chicken",
+            "diet":["keto", "gluten_free", "none"]
+        },
+        11:{
+            "name":"Thai Quinoa Salad",
+            "ingredients":["quinoa", "carrot", "red cabbage", "scallions", "cilantro", "basil", "jalapeno", "roasted peanuts", "olive oil", "lime juice", "soy sauce", "honey"],
+            "cuisine":"Asian",
+            "protein":"quinoa",
+            "diet":["vegetarian", "none", "gluten_free"]
+        },
+        12:{
+            "name":"Quinoa Crust Pizza",
+            "ingredients":["quinoa", "egg", "parmigiano reggiano", "baking powder", "dried oregano", "marinara sauce", "mozzarella cheese"],
+            "cuisine":"Italian",
+            "protein":"quinoa",
+            "diet":["vegetarian", "gluten_free"]
+        },
+        13:{
+            "name":"Cheesey Beef and Caramelized Onion Quesadilla",
+            "ingredients":["ground beef", "bbq spice rub", "olive oil", "tortillas", "mozzarella cheese", "yellow onion", "cheddar cheese", "hot sauce", "avocado", "jalapeno", "sour cream"],
+            "cuisine":"Tex Mex",
+            "protein":"beef",
+            "diet":["gluten_free", "none"]
+        },
+        14:{
+            "name":"Balsamic Steak Salad",
+            "ingredients":["skirt steak", "balsamic vinegar", "olive oil", "garlic", "lettuce", "tomatoes", "fresh mozzarella", "red onion", "avocado", "parmigiano reggiano"],
+            "cuisine":"Italian",
+            "protein":"beef",
+            "diet":["keto", "gluten_free", "none"]
+        },
+        15:{
+            "name":"Marinated Skirt Steak Salad",
+            "ingredients":["skirt steak", "olive oil", "lemon", "soy sauce", "worchestershire sauce", "dijon mustard", "garlic", "fire roasted red pepper", "arugula", "asiago cheese", "lettuce"],
+            "cuisine":"American",
+            "protein":"beef",
+            "diet":["keto", "gluten_free", "none"]
+        },
+        16:{
+            "name":"Sausage Skewers",
+            "ingredients":["hot italian sausage", "yellow onion", "red bell pepper", "orange bell pepper", "yellow bell pepper", "zucchini", "tomatoes"],
+            "cuisine":"American",
+            "protein":"sausage",
+            "diet":["keto", "gluten_free", "none"]
+        },
+        17:{
+            "name":"Egg Roll in a Bowl",
+            "ingredients":["ground pork", "green cabbage", "yellow onion", "carrot", "scallions", "soy sauce", "srirachia", "ginger", "garlic", "eggs"],
+            "cuisine":"Asian",
+            "protein":"pork",
+            "diet":["keto"]
+        },
+        18:{
+            "name":"Pad Thai",
+            "ingredients":["rice noodles", "firm tofu", "red bell pepper", "carrot", "yellow onion", "bean sprouts", "roasted peanuts", "cilantro", "soy sauce", "lime juice", "palm sugar"],
+            "cuisine":"Asian",
+            "protein":"tofu",
+            "diet":["vegetarian"]
+        },
+        19:{
+            "name":"Supreme Cobb Salad",
+            "ingredients":["chicken", "bacon", "eggs", "ham", "cheddar cheese", "lettuce", "red onion", "avocado","sour cream", "heavy whipping cream", "scallions", "garlic", "parsley", "dill", "lemon", "paprika"],
+            "cuisine":"American",
+            "protein":"chicken",
+            "diet":["keto", "gluten_free", "none"]
+        },
+        20:{
+            "name":"Keto Chicken Parmesan with Zoodles",
+            "ingredients":["chicken breast", "almond flour", "parmiggano reggianio", "dried minced onion", "egg", "heavy cream", "tomato sauce", "fresh mozzarella", "zucchini", "italian seasoning"],
+            "cuisine":"Italian",
+            "protein":"chicken",
+            "diet":["keto"]
+        },
+        21:{
+            "name":"Thai Basil Beef with Asian Slaw",
+            "ingredients":["ground beef", "serrano pepper", "garlic", "soy sauce", "oyster sauce", "basil", "shallot", "red cabbage", "green cabbage", "scallions", "carrot", "lime", "fish sauce", "red pepper flake", "sesame oil", "sesame seeds"],
+            "cuisine":"Asian",
+            "protein":"beef",
+            "diet":["keto", "none", "gluten_free"]
+        },
+        22:{
+            "name":"Keto Pizza Chicken Roulade",
+            "ingredients":["chicken breast", "eggs", "heavy cream", "almond flour", "parmiggano reggiano", "italian seasoning", "pepperoni", "provolone cheese", "tomato sauce", "mozzarella cheese"],
+            "cuisine":"Italian",
+            "protein":"chicken",
+            "diet":["keto"]
+        },
+        23:{
+            "name":"Ribeye Steak with CauliMash and Asparagus",
+            "ingredients":["ribeye steak", "cauliflower", "cream cheese", "butter", "garlic", "parmiggano reggiano", "asparagus"],
+            "cuisine":"American",
+            "protein":"beef",
+            "diet":["keto"]
+        },
+        24:{
+            "name":"Beef with Broccoli",
+            "ingredients":["ribeye steak", "soy sauce", "corn starch", "garlic", "ginger", "srirachia", "broccoli", "sesame oil", "oyster sauce"],
+            "cuisine":"Asian",
+            "protein":"beef",
+            "diet":["keto", "gluten_free", "none"]
+        },
+        25:{
+            "name":"Pesto Linguini",
+            "ingredients":["basil", "lemon", "parmiggano reggiano", "olive oil", "garlic", "almonds", "linguini"],
+            "cuisine":"Italian",
+            "protein":"",
+            "diet":["vegetarian"]
+        },
+        26:{
+            "name":"Parmesan Focaccia with Tomato Basil Soup",
+            "ingredients":["bread flour", "potato", "yeast", "olive oil", "parmiggano reggiano", "canned san marzano tomatoes", "garlic", "white rice", "basil", "sweet onion", "coconut milk"],
+            "cuisine":"Italian",
+            "protein":"",
+            "diet":["vegetarian"]
+        },
+        27:{
+            "name":"Harvest Buddah Bowl",
+            "ingredients":["quinoa", "kale", "sweet potato", "apple", "pomegranate seeds", "lemon", "olive oil", "tahini"],
+            "cuisine":"American",
+            "protein":"quinoa",
+            "diet":["vegetarian"]
+        },
+        28:{
+            "name":"Crispy Chickpea and Broccolini Buddah Bowl with Buckwheat Crepes",
+            "ingredients":["canned chickpeas", "olive oil", "broccolini", "sweet potato", "buckwheat flour", "almond milk", "eggs", "tahini", "lemon"],
+            "cuisine":"American",
+            "protein":"chickpeas",
+            "diet":["vegetarian"]
+        },
+        29:{
+            "name":"Penne Vodka",
+            "ingredients":["penne pasta", "garlic", "yellow onion", "butter", "olive oil", "vodka", "canned san marzano tomatoes", "heavy cream", "red pepper flake", "parmesan"],
+            "cuisine":"Italian",
+            "protein":"",
+            "diet":["vegetarian", "gluten_free", "none"]
+        },
+        30:{
+            "name":"Veggie Enchiladas",
+            "ingredients":["corn tortillas", "yellow bell pepper", "zucchini", "yellow onion", "cream cheese", "cream cheese", "enchilada sauce", "cheddar cheese", "avocado", "lettuce", "tomatoes", "jalapeno", "lime"],
+            "cuisine":"Mexican",
+            "protein":"",
+            "diet":["vegetarian"]
+        },
+        31:{
+            "name":"Sweet and Spicy Ginger Soy Noodles",
+            "ingredients":["rice noodles", "soy sauce", "honey", "rice vinegar", "balsamic vinegar", "peanut butter", "ginger", "garlic", "vegetable broth", "kale", "sesame oil", "shiitake mushrooms", "shallot", "peanuts", "red pepper flakes", "scallions"],
+            "cuisine":"Asian",
+            "protein":"",
+            "diet":["vegetarian"]
+        },
+        32:{
+            "name":"Avocado Sweet Potato Toast",
+            "ingredients":["sweet potato", "olive oil", "avocado", "lemon", "microgreens", "cucumber", "pepitas"],
+            "cuisine":"American",
+            "protein":"",
+            "diet":["vegetarian"]
+        }
+}
